4abf64bb9379064f766668b731ae73843969fd54
Arthur Miasato Pimont <arthur.miasatopimont@uel.br>
2021-03-28 23:36:40 -0300

    Atualização de comentários

--- args.py
+++ args.py
@@ -1,6 +1,6 @@
 class Args:
     def __init__(self, arguments):
-        self.np = int(arguments[1])
-        self.min_pot = int(arguments[2])
-        self.max_pot = int(arguments[3])
-        self.msg = " ".join(arguments[4:])
+        self.np = int(arguments[1])             # Quantidade de primos utilizados
+        self.min_pot = int(arguments[2])        # Limitante inferior para geração dos primos (10**min_pot)
+        self.max_pot = int(arguments[3])        # Limitante superior para geração dos primos (10**max_pot)
+        self.msg = " ".join(arguments[4:])      # Mensagem que será utilizada
